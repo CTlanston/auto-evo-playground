23e6d73916319eccf736f554f2469be7af47876d
Coder <coder@shadow-ci.local>
2026-05-16 01:22:34 +0000

    feat: export truncate from src package __all__

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from src.telemetry_aggregator import aggregate_metrics
 from src.run_recorder import record_run, RunStore
+from src.utils import truncate
 
-__all__ = ["aggregate_metrics", "record_run", "RunStore"]
+__all__ = ["aggregate_metrics", "record_run", "RunStore", "truncate"]
