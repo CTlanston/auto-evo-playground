9b6bc377bc6cc9223c401b37667203b9d1e403e8
Shadow Coder <coder@shadow.ai>
2026-05-16 00:17:49 +0000

    feat: export is_palindrome from src.__init__ so callers can do `from src import is_palindrome`

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from src.telemetry_aggregator import aggregate_metrics
 from src.run_recorder import record_run, RunStore
+from src.utils import is_palindrome
 
-__all__ = ["aggregate_metrics", "record_run", "RunStore"]
+__all__ = ["aggregate_metrics", "record_run", "RunStore", "is_palindrome"]
